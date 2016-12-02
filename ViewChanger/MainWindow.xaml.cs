6455bb37b42ea7015d6f5d5f938fccf3e1c7b6c4
Tosker <josephebenge@gmail.com>
2016-12-02 16:39:45 -0500

    Importing WPF example

---
+++ ViewChanger/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ViewChanger.ViewModels;
+
+namespace ViewChanger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RedView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new RedViewModel();
+        }
+
+        private void BlueView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new BlueViewModel();
+        }
+
+        private void OrangeView_Clicked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new OrangeViewModel();
+        }
+    }
+}
